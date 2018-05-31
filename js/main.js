1e6d8d0ec2ffeb82de34ab18f30a7a703aa4b368
Matt Elia <q@Matts-MacBook-Air.local>
2018-05-31 13:47:18 -0400

    v2-parallax update w/pic

--- js/main.js
+++ js/main.js
@@ -1,41 +1,3 @@
-// $(document).ready(function () {
-	
-// 	setTimeout(function () {
-// 		$('#blue').addClass('fadeInUpBig');
-// 		setTimeout(function () {
-// 			$('#diamond-blue').addClass('fadeIn');
-// 			$('body').removeClass('overflow-hidden');
-// 		}, 1000);
-// 	}, 500);
-
-// 	$(window).scroll(function() {
-// 		var hTone = $('#one').offset().top,
-// 				hHone = $('#one').outerHeight(),
-// 				hTtwo = $('#two').offset().top,
-// 				hHtwo = $('#two').outerHeight(),
-// 				hTthree = ($('#three').offset().top-420),
-// 				hHthree = $('#three').outerHeight(),
-// 				hTfour = $('#four').offset().top,
-// 				hHfour = $('#four').outerHeight(),
-// 				wH = $(window).height(),
-// 				wS = $(this).scrollTop();
-// 				// console.log((hT-wH) , wS);
-
-// 		if (wS > (hTone+hHone-wH)){
-// 			$('#yellow').addClass('fadeInDownBig');
-// 		}
-// 		if (wS > (hTtwo+hHtwo-wH)){
-// 			$('#green').addClass('rotateInDownLeft');
-// 		}
-// 		if (wS > (hTthree+hHthree-wH)){
-// 			$('#pink').addClass('fadeInLeftBig');
-// 		}
-// 		if (wS > (hTfour+hHfour-wH)){
-// 			$('#pink-splash').addClass('fadeInLeftBig');
-// 		}
-// 	});
-// })
-
 $(document).ready(function(){
 	$('#nav').localScroll(800);
 	
@@ -43,14 +5,16 @@ $(document).ready(function(){
 	//xPosition - Horizontal position of the element
 	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
 	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
-	$('#intro').parallax("50%", 0.1);
 	$('#second').parallax("50%", 0.1);
-	$('.bg').parallax("50%", 0.4);
+	$('.bg').parallax("50%", 0.6);
 	$('.bg2').parallax("50%", 0.2);
-	$('.bg3').parallax("50%", 0.3);
-	$('.bg4').parallax("50%", 0.7);
-	$('.bg5').parallax("50%", 0.1);
+	$('.bg3').parallax("50%", 0.8);
+	$('.bg4').parallax("50%", 0.4);
+	$('.bg5').parallax("50%", 0.5);
 	$('.bg6').parallax("50%", 0.3);
-	$('#third').parallax("50%", 0.3);
+
+	setTimeout(function () {
+		$('#diamond-blue').addClass('fadeIn');
+	}, 500);
 
 });
