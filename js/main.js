99fbe570b86b8428c3aa3c530ffe249b22f579c7
Matt Elia <q@Matts-MacBook-Air.local>
2018-06-01 13:20:31 -0400

    carousel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-	$('#nav').localScroll(800);
 	
 	//.parallax(xPosition, speedFactor, outerHeight) options:
 	//xPosition - Horizontal position of the element
@@ -9,26 +8,9 @@ $(document).ready(function(){
 	$('.bg').parallax("50%", 0.7);
 	$('.bg2').parallax("50%", 0.2);
 	$('.bg3').parallax("50%", 0.5);
-	$('.bg4').parallax("50%", 0.4);
-	$('.bg5').parallax("50%", 0.5);
-	$('.bg6').parallax("50%", 0.3);
-
-	$.fn.animateRotate = function(angle, duration, easing, complete) {
-		var args = $.speed(duration, easing, complete);
-		var step = args.step;
-		return this.each(function(i, e) {
-			args.step = function(now) {
-			  $.style(e, 'transform', 'rotate(' + now + 'deg)');
-			  if (step) return step.apply(this, arguments);
-			};
-
-			$({deg: 0}).animate({deg: angle}, args);
-		});
-	};
-
-	// $('.bg3').animateRotate(1080, 300000);
+	$('.bg4').parallax("50%", -0.1);
+	$('.bg5').parallax("50%", -0.5);
 	
-
 	setTimeout(function () {
 		$('#diamond-blue').addClass('fadeIn');
 	}, 500);
@@ -46,7 +28,6 @@ $(document).ready(function(){
 	$('.bg3').css('height', ''+totalHeight+'');
 	$('.bg4').css('height', ''+totalHeight+'');
 	$('.bg5').css('height', ''+totalHeight+'');
-	$('.bg6').css('height', ''+totalHeight+'');
 
 	$(window).resize(function () {
 
@@ -63,7 +44,7 @@ $(document).ready(function(){
 		$('.bg3').css('height', ''+totalHeight+'');
 		$('.bg4').css('height', ''+totalHeight+'');
 		$('.bg5').css('height', ''+totalHeight+'');
-		$('.bg6').css('height', ''+totalHeight+'');
 
 	});
+
 });
